959cde69e1ec3a75ad3e9c4fcb52f8a3a8576c0c
unknown <chris.chen@YFY.CORP>
2020-05-27 15:49:56 +0800

    加入專案檔案。

---
+++ socketSeerver/FormServer.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace socketSeerver
+{
+    partial class FormServer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_ip = new System.Windows.Forms.TextBox();
+            this.textBox_port = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(444, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "啟動";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(395, 263);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 77);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "發送";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(227, 263);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(162, 20);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("標楷體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("標楷體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(247, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "PORT：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("標楷體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(227, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Client：";
+            // 
+            // textBox_ip
+            // 
+            this.textBox_ip.Location = new System.Drawing.Point(76, 9);
+            this.textBox_ip.Name = "textBox_ip";
+            this.textBox_ip.Size = new System.Drawing.Size(145, 22);
+            this.textBox_ip.TabIndex = 6;
+            // 
+            // textBox_port
+            // 
+            this.textBox_port.Location = new System.Drawing.Point(335, 9);
+            this.textBox_port.Name = "textBox_port";
+            this.textBox_port.Size = new System.Drawing.Size(100, 22);
+            this.textBox_port.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 70);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(419, 134);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(16, 236);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(205, 104);
+            this.textBox2.TabIndex = 9;
+            // 
+            // FormServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 352);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox_port);
+            this.Controls.Add(this.textBox_ip);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "FormServer";
+            this.Text = "Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_ip;
+        private System.Windows.Forms.TextBox textBox_port;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ socketSeerver/FormServer.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace socketSeerver
+{
+    public partial class FormServer : Form
+    {
+        Socket socketListen;//用於監聽的socket
+        Socket socketConnect;//用於通訊的socket
+        string RemoteEndPoint;     //客戶端的網路節點 
+        Dictionary<string, Socket> dicClient = new Dictionary<string, Socket>();//連線的客戶端集合
+
+
+        public FormServer()
+        {
+            InitializeComponent();
+            //Control.CheckForIllegalCrossThreadCalls = false;
+            //comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            textBox1.Multiline = true;
+            textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StartSocket();
+        }
+
+        public void StartSocket()
+        {
+            //建立套接字
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Parse(textBox_ip.Text), int.Parse(textBox_port.Text));
+            socketListen = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //繫結埠和IP
+            socketListen.Bind(ipe);
+            //設定監聽
+            socketListen.Listen(10);
+            //連線客戶端
+            AsyncConnect(socketListen);
+        }
+
+        /// <summary>
+        /// 連線到客戶端
+        /// </summary>
+        /// <param name="socket"></param>
+        private void AsyncConnect(Socket socket)
+        {
+            try
+            {
+                socket.BeginAccept(asyncResult =>
+                    {
+                        //獲取客戶端套接字
+                        socketConnect = socket.EndAccept(asyncResult);
+                        RemoteEndPoint = socketConnect.RemoteEndPoint.ToString();
+                        dicClient.Add(RemoteEndPoint, socketConnect);//新增至客戶端集合
+                        comboBox1.Items.Add(RemoteEndPoint);//新增客戶端埠號
+                        AsyncSend(socketConnect, string.Format("歡迎你{0}", socketConnect.RemoteEndPoint));
+                        AsyncReceive(socketConnect);
+                        AsyncConnect(socketListen);
+                    }, null);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void AsyncReceive(Socket socket)
+        {
+            byte[] data = new byte[1024];
+            try
+            {
+                socket.BeginReceive(data, 0, data.Length, SocketFlags.None, asyncResult =>
+                    {
+                        try
+                        {
+                            int length = socket.EndReceive(asyncResult);
+                            setText(Encoding.UTF8.GetString(data));
+                        }
+                        catch(Exception ex)
+                        {
+                            AsyncReceive(socket);
+                        }
+                        AsyncReceive(socket);
+                    }, null);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void AsyncSend(Socket client, string message)
+        {
+            if (client == null || message == string.Empty) return;
+            //資料轉碼
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            try
+            {
+                //開始傳送訊息
+                client.BeginSend(data, 0, data.Length, SocketFlags.None, asyncResult =>
+                {
+                    //完成訊息傳送
+                    int length = client.EndSend(asyncResult);
+                }, null);
+            }
+            catch (Exception ex)
+            {
+                //傳送失敗，將該客戶端資訊刪除
+                string deleteClient = client.RemoteEndPoint.ToString();
+                dicClient.Remove(deleteClient);
+                comboBox1.Items.Remove(deleteClient);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == -1)
+            {
+                AsyncSend(socketConnect, textBox2.Text);
+            }
+            else
+            {
+                AsyncSend(dicClient[comboBox1.SelectedItem.ToString()], textBox2.Text);
+            }
+        }
+
+        private void setText(string str)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new MethodInvoker(() => setText(str)));
+            }
+            else
+            {
+                //textBox1.Text += "\r\n" + str;
+                textBox1.AppendText("\r\n" + str + Environment.NewLine);
+            }
+        }
+    }
+}
